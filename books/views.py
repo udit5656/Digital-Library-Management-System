acade4dea86fd8efa3609f59d39287fa6e530121
udit5656 <uditgarg5656@gmail.com>
2020-08-24 18:01:46 +0530

    Added Book search result page

--- books/views.py
+++ books/views.py
@@ -14,11 +14,12 @@ def home(request):
         form = BookSearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['q']
+            profile = User.objects.get(username=request.user.username).profile
             result = Book.objects.all().filter(
                 Q(title__icontains=query) | Q(author_name__icontains=query) | Q(publisher_name__icontains=query) | Q(
                     pub_year__icontains=query))
-            context = {'result': result}
-            return render(request, 'book/result.html', context)
+            context = {'result': result, 'profile': profile}
+            return render(request, 'books/result.html', context)
         profile = User.objects.get(username=request.user.username).profile
         context = {'profile': profile, 'form': form}
         return render(request, 'books/home.html', context=context)
