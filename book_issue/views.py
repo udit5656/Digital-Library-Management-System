644c3232766770418596194b2be176973b3d9fe4
udit5656 <uditgarg5656@gmail.com>
2020-08-26 00:51:12 +0530

    Fixed Book issue code not working properly bug

--- book_issue/views.py
+++ book_issue/views.py
@@ -7,7 +7,14 @@ Book = apps.get_model('books', 'Book')
 
 # Create your views here.
 def issue(request, book_id):
-    book_issue_code = BookIssueCode.create(user=request.user, book=Book.objects.get(pk=book_id))
-    book_issue_code.save()
-    context = {'book_issue_code': book_issue_code}
+    book = Book.objects.get(pk=book_id)
+    if book.check_availability():
+        book_issue_code = BookIssueCode.create(user=request.user, book=book)
+        book_issue_code.save()
+        book.books_available = book.books_available - 1
+        book.save()
+        context = {'error': None, 'book_id': book_id, 'book_issue_code': book_issue_code}
+    else:
+        error = "This book is currently not available. Please try later."
+        context = {'error': error, 'book_id': book_id, 'book_issue_code': None}
     return render(request, 'book_issue/issue.html', context)
