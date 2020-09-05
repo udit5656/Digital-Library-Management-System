bffa4d9aae514fca2b3752aa5ad51e77e9b142bf
udit5656 <uditgarg5656@gmail.com>
2020-09-05 16:37:52 +0530

    Minor Changes in urls settings

--- LibraryManagement/urls.py
+++ LibraryManagement/urls.py
@@ -17,8 +17,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='accounts/login/')),
     path('admin/', admin.site.urls),
     path('books/', include('books.urls')),
     path('accounts/', include('accounts.urls')),
